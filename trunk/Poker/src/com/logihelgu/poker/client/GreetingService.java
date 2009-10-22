7f4c3158b6e129415122b79a1094156fb88785a4
logihelgu <logihelgu@bbc24f04-befd-11de-91a4-794fdb80bb57>
2009-10-22 12:51:41 +0000

    Initial import

---
+++ trunk/Poker/src/com/logihelgu/poker/client/GreetingService.java
@@ -0,0 +1,11 @@
+package com.logihelgu.poker.client;
+
+import com.google.gwt.user.client.rpc.*;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath( "greet" )
+public interface GreetingService extends RemoteService {
+	String greetServer( String name );
+}
